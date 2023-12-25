e48db6ac7f0fd9ca9ef41579d0c616fc268748ee
alamindesign <mail.muhammadit@gmail.com>
2023-12-25 18:55:27 +0600

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'samantha',
+        email: 'nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Labsack',
+        username: 'karianne',
+        email: 'Julianne.oconner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'kamren',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'karley_dach@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.skiles',
+        email: 'telly.hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'maxime_nienow',
+        email: 'sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'delphine',
+        email: 'chaim_mcdermott@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'moriah.stanton',
+        email: 'rey.padberg@karina.biz'
+    },
+]
